2dc3dff88b6934b7841a864c336c2e5db49ba0f0
mszalbach <4-bar-killa@web.de>
2017-07-05 14:46:27 +0200

    - moved https config to webpack.config instead pf package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
-const path = require('path');
-const webpack = require('webpack');
-const merge = require('webpack-merge');
-const pkg = require("./package.json");
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require( 'path' );
+const webpack = require( 'webpack' );
+const merge = require( 'webpack-merge' );
+const pkg = require( "./package.json" );
+const CleanWebpackPlugin = require( 'clean-webpack-plugin' );
+const ExtractTextPlugin = require( 'extract-text-webpack-plugin' );
+const HtmlWebpackPlugin = require( 'html-webpack-plugin' );
 
 
 const PATHS = {
-    app: path.join(__dirname, 'src/main/js'),
-    build: path.join(__dirname, 'target/build')
+    app: path.join( __dirname, 'src/main/js' ),
+    build: path.join( __dirname, 'target/build' )
 };
 
 
 const common = {
     entry: {
         app: PATHS.app,
-        vendor: Object.keys(pkg.dependencies)
+        vendor: Object.keys( pkg.dependencies )
     },
     devtool: 'source-map',
     output: {
@@ -49,18 +49,20 @@ const common = {
         ]
     },
 
-    devServer: {},
+    devServer: {
+        https: true
+    },
 
 
     plugins: [
-        new ExtractTextPlugin('bundle.css'),
-        new HtmlWebpackPlugin({
-            template: './src/main/js/index.html'
-        }),
-        new webpack.ProvidePlugin({
-            $: "jquery",
-            jQuery: "jquery"
-        })
+        new ExtractTextPlugin( 'bundle.css' ),
+        new HtmlWebpackPlugin( {
+                                   template: './src/main/js/index.html'
+                               } ),
+        new webpack.ProvidePlugin( {
+                                       $: "jquery",
+                                       jQuery: "jquery"
+                                   } )
     ]
 };
 
@@ -68,32 +70,32 @@ const common = {
 let config;
 
 // Detect how npm is run and branch based on that
-switch (process.env.npm_lifecycle_event) {
+switch ( process.env.npm_lifecycle_event ) {
     case 'build':
-        config = merge(common, {
+        config = merge( common, {
             output: {
                 filename: '[name].[chunkhash].js'
             },
             devtool: 'cheap-source-map',
-            plugins: [new webpack.DefinePlugin({
-                'process.env.NODE_ENV': JSON.stringify('production')
+            plugins: [new webpack.DefinePlugin( {
+                                                    'process.env.NODE_ENV': JSON.stringify( 'production' )
 
-            }),
-                new webpack.optimize.UglifyJsPlugin({
-                    compress: {
-                        warnings: false
-                    }
-                }),
-                new webpack.optimize.CommonsChunkPlugin({name: "vendor", filename: "[name].[chunkhash].js"}),
-                new CleanWebpackPlugin(PATHS.build)
+                                                } ),
+                      new webpack.optimize.UglifyJsPlugin( {
+                                                               compress: {
+                                                                   warnings: false
+                                                               }
+                                                           } ),
+                      new webpack.optimize.CommonsChunkPlugin( {name: "vendor", filename: "[name].[chunkhash].js"} ),
+                      new CleanWebpackPlugin( PATHS.build )
             ]
 
-        });
+        } );
         break;
     default:
-        config = merge(common, {
-                devtool: 'eval-source-map'
-            }
+        config = merge( common, {
+                            devtool: 'eval-source-map'
+                        }
         );
 
 }
