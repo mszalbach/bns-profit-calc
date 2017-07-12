b60b2079bee33668ac1093888b5d5d542c741f4b
mszalbach <4-bar-killa@web.de>
2017-07-12 10:10:23 +0200

    - changed bidPrices to use BootstrapTable

--- src/main/js/components/bidPrices/bidfPrices.jsx
+++ src/main/js/components/bidPrices/bidfPrices.jsx
@@ -1,82 +1,62 @@
 import React from "react";
-import {ControlLabel, Form, FormGroup, Table} from "react-bootstrap";
-import PropTypes from "prop-types";
+import {ControlLabel, Form, FormGroup} from "react-bootstrap";
 import PriceEdit from "../priceEdit";
 import PriceText from "../priceText";
-
-import "./bidPrices.css";
+import {BootstrapTable, TableHeaderColumn} from "react-bootstrap-table";
 
 
 export default class BidPrices extends React.Component {
 
     state = {
-        bidPrice: 100000,
-    };
-
-    playerHeader = ( start, end ) => {
-        let options = [];
-        for ( let i = start; i <= end; i++ ) {
-            options.push( <th key={i} value={i}>{i}</th> );
-        }
-        return options;
+        itemPrice: 100000,
     };
 
 
     onChangePrice = ( name, value ) => {
-        this.setState( {bidPrice: value} );
+        this.setState( {itemPrice: value} );
     };
 
+    priceFormatter( cell ) {
+        return <PriceText price={cell}/>
+    }
+
+    calculateBidPrices = () => {
+        return [...new Array( 20 ).keys()].map( i => {
+            return {
+                itemCount: i + 1,
+                players2Price: Math.floor( (i + 1) * this.state.itemPrice * (1 / 2) ),
+                players3Price: Math.floor( (i + 1) * this.state.itemPrice * (2 / 3) ),
+                players4Price: Math.floor( (i + 1) * this.state.itemPrice * (3 / 4) ),
+                players5Price: Math.floor( (i + 1) * this.state.itemPrice * (4 / 5) ),
+                players6Price: Math.floor( (i + 1) * this.state.itemPrice * (5 / 6) )
+            }
+        } );
+    };
 
     render() {
+
+
         return <div><Form inline>
             <FormGroup>
                 <ControlLabel>Price</ControlLabel>
                 {' '}
-                <PriceEdit name="price" price={this.state.bidPrice} mergePrice={this.onChangePrice}/>
+                <PriceEdit name="price" price={this.state.itemPrice} mergePrice={this.onChangePrice}/>
             </FormGroup>
             {' '}
         </Form>
             <br/>
-            <Table striped bordered condensed hover>
-                <thead>
-                <tr>
-                    <th># / Players</th>
-                    <th className="right">6</th>
-                    <th className="right">5</th>
-                    <th className="right">4</th>
-                    <th className="right">3</th>
-                    <th className="right">2</th>
-                </tr>
-                </thead>
-                <tbody>
-                {[...new Array( 20 ).keys()].map(
-                    i => <BidPriceTableRow key={i} itemPrice={this.state.bidPrice} itemCount={i + 1}/> )}
-                </tbody>
-            </Table>
+            <BootstrapTable data={this.calculateBidPrices()}
+                            striped={true}
+                            hover={true}
+            >
+                <TableHeaderColumn dataField="itemCount" isKey={true} tdStyle={ {fontWeight: 'bold'} }
+                                   dataAlign="right">#</TableHeaderColumn>
+                {[...new Array( 5 ).keys()].reverse().map(
+                    players => <TableHeaderColumn key={players} dataField={"players" + (players + 2) + "Price"}
+                                                  dataFormat={this.priceFormatter} dataAlign="right">{(players + 2)
+                                                                                                      + " Players"}
+                    </TableHeaderColumn> )}
+            </BootstrapTable>
         </div>
-    }
-}
-
-class BidPriceTableRow
-    extends React
-        .Component {
-
-    static propTypes = {
-        itemCount: PropTypes.number.isRequired,
-        itemPrice: PropTypes.number.isRequired
-    };
-
-    calculateBidPrice = ( playersCount ) => {
-        let {itemPrice, itemCount} = this.props;
-        return Math.floor( itemCount * itemPrice * ((playersCount - 1) / playersCount) );
-    };
-
-
-    render() {
-        return <tr>
-            <th>{this.props.itemCount}</th>
-            {[...new Array( 5 ).keys()].reverse().map(
-                i => <td key={i} className="right"><PriceText price={this.calculateBidPrice( i + 2 )}/></td> )}
-        </tr>
     }
 }
