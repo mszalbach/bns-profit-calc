b61dafd6c42de15d6189980ada63f85dffc8319c
mszalbach <4-bar-killa@web.de>
2017-07-12 09:36:38 +0200

    - changed bidprices to a table instead of 3 form elements

--- src/main/js/components/bidPrices/bidfPrices.jsx
+++ src/main/js/components/bidPrices/bidfPrices.jsx
@@ -1,79 +1,82 @@
 import React from "react";
-import {ControlLabel, Form, FormControl, FormGroup} from "react-bootstrap";
-import PriceText from "../priceText";
+import {ControlLabel, Form, FormGroup, Table} from "react-bootstrap";
+import PropTypes from "prop-types";
 import PriceEdit from "../priceEdit";
+import PriceText from "../priceText";
+
+import "./bidPrices.css";
 
 
 export default class BidPrices extends React.Component {
 
     state = {
-        itemCount: 1,
         bidPrice: 100000,
-        players: 6
     };
 
-    onChangeCount = ( e ) => {
-        this.setState( {itemCount: e.target.value} );
+    playerHeader = ( start, end ) => {
+        let options = [];
+        for ( let i = start; i <= end; i++ ) {
+            options.push( <th key={i} value={i}>{i}</th> );
+        }
+        return options;
     };
+
 
     onChangePrice = ( name, value ) => {
         this.setState( {bidPrice: value} );
     };
 
-    onChangePlayers = ( e ) => {
-        this.setState( {players: e.target.value} );
-    };
-
-    calculateBidPrice = () => {
-        let {itemCount, bidPrice, players} = this.state;
-        return Math.floor( itemCount * bidPrice * ((players - 1) / players) );
-    };
-
-    countOptions = ( start, end ) => {
-        let options = [];
-        for ( let i = start; i <= end; i++ ) {
-            options.push( <option key={i} value={i}>{i}</option> );
-        }
-        return options;
-    };
 
     render() {
         return <div><Form inline>
-            <FormGroup>
-                <ControlLabel>Count</ControlLabel>
-                {' '}
-                <FormControl bsSize="small"
-                             type="number"
-                             componentClass="select"
-                             value={this.state.itemCount}
-                             onChange={this.onChangeCount}>
-                    {this.countOptions( 1, 20 )}
-                </FormControl>
-            </FormGroup>
-            {' '}
             <FormGroup>
                 <ControlLabel>Price</ControlLabel>
                 {' '}
                 <PriceEdit name="price" price={this.state.bidPrice} mergePrice={this.onChangePrice}/>
             </FormGroup>
             {' '}
-            <FormGroup>
-                <ControlLabel>Players</ControlLabel>
-                {' '}
-                <FormControl bsSize="small"
-                             type="number"
-                             componentClass="select"
-                             value={this.state.players}
-                             onChange={this.onChangePlayers}>
-                    {this.countOptions( 2, 6 ).reverse()}
-                </FormControl>
-            </FormGroup>
         </Form>
             <br/>
-            <FormGroup>
-                <ControlLabel>Max Bid price</ControlLabel>
-                {' '}
-                <PriceText price={this.calculateBidPrice()}/>
-            </FormGroup></div>
+            <Table striped bordered condensed hover>
+                <thead>
+                <tr>
+                    <th># / Players</th>
+                    <th className="right">6</th>
+                    <th className="right">5</th>
+                    <th className="right">4</th>
+                    <th className="right">3</th>
+                    <th className="right">2</th>
+                </tr>
+                </thead>
+                <tbody>
+                {[...new Array( 20 ).keys()].map(
+                    i => <BidPriceTableRow key={i} itemPrice={this.state.bidPrice} itemCount={i + 1}/> )}
+                </tbody>
+            </Table>
+        </div>
+    }
+}
+
+class BidPriceTableRow
+    extends React
+        .Component {
+
+    static propTypes = {
+        itemCount: PropTypes.number.isRequired,
+        itemPrice: PropTypes.number.isRequired
+    };
+
+    calculateBidPrice = ( playersCount ) => {
+        let {itemPrice, itemCount} = this.props;
+        return Math.floor( itemCount * itemPrice * ((playersCount - 1) / playersCount) );
+    };
+
+
+    render() {
+        return <tr>
+            <th>{this.props.itemCount}</th>
+            {[...new Array( 5 ).keys()].reverse().map(
+                i => <td key={i} className="right"><PriceText price={this.calculateBidPrice( i + 2 )}/></td> )}
+        </tr>
     }
 }
