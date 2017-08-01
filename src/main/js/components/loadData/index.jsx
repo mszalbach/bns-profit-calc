618113bb79bf6ac928342f09adbcf8d47beaed70
mszalbach <4-bar-killa@web.de>
2017-08-01 15:32:45 +0200

    - added loading from server every minute

--- src/main/js/components/loadData/index.jsx
+++ src/main/js/components/loadData/index.jsx
@@ -3,9 +3,23 @@ import React from "react";
 
 export default class LoadData extends React.Component {
 
-    componentDidMount() {
+    state = {
+        timer: null,
+    };
+    loadDataFromServer = () => {
         this.props.loadItems();
         this.props.loadPrices();
+    };
+
+    componentDidMount() {
+        this.loadDataFromServer();
+        let timer = setInterval( this.loadDataFromServer, 60 * 1000 );
+
+        this.setState( {timer: timer} );
+    }
+
+    componentWillUnmount() {
+        this.clearInterval( this.state.timer );
     }
 
     render() {
