196492701a2f4842b00192da6268f5a88d98f427
mszalbach <4-bar-killa@web.de>
2017-08-04 10:47:40 +0200

    - corrected test to new axios middleware usage

--- src/test/js/modules/prices/pricesReducer.test.jsx
+++ src/test/js/modules/prices/pricesReducer.test.jsx
@@ -1,25 +1,21 @@
-import pricesReducer, {PRICES_CLEAR, PRICES_LOAD} from "../../../../main/js/modules/prices/pricesReducer";
+import pricesReducer, {LOADED_PRICES_FROM_SERVER} from "../../../../main/js/modules/prices/pricesReducer";
 
 describe( 'Price Reducer', () => {
     it( 'should load single item to empty state', () => {
-        expect( pricesReducer( [], {type: PRICES_LOAD, items: [{name: "item1", price: 5}]} ) ).toEqual(
+        expect( pricesReducer( [], {
+            type: LOADED_PRICES_FROM_SERVER,
+            payload: {data: [{name: "item1", price: 5}]}
+        } ) ).toEqual(
             [{name: 'item1', price: 5}] )
     } );
 
     it( 'should overwrite item in state with new one', () => {
         expect( pricesReducer( [{name: 'item1', "listings": [{"price": 6, "count": 1}]}],
                                {
-                                   type: PRICES_LOAD,
-                                   items: [{name: "item1", "listings": [{"price": 10, "count": 2}]}]
+                                   type: LOADED_PRICES_FROM_SERVER,
+                                   payload: {data: [{name: "item1", "listings": [{"price": 10, "count": 2}]}]}
                                } ) ).toEqual(
             [{name: 'item1', "listings": [{"price": 10, "count": 2}]}] )
     } );
-
-    it( 'should set state to empty when cleared', () => {
-        expect(
-            pricesReducer( [{name: 'item1', "listings": [{"price": 5, "count": 1}]}], {type: PRICES_CLEAR} ) ).toEqual(
-            [] )
-    } );
-
 
 } );
