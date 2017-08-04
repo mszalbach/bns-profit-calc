79dc570e6f87460b142eaa4d441a6ef90f897c48
mszalbach <4-bar-killa@web.de>
2017-08-04 11:14:49 +0200

    - added BASEURL to ITEMSURL

--- src/main/js/modules/region/regionSelector.js
+++ src/main/js/modules/region/regionSelector.js
@@ -8,4 +8,4 @@ export const getRegionSelector = ( state ) => state.region;
 export const getPricesUrlSelector = createSelector( [getRegionSelector],
                                                     ( region ) => BASEURL + PRICES_URL.replace( '{region}', region ) );
 
-export const getItemsUrlSelector = ( state ) => ITEMS_URL;
+export const getItemsUrlSelector = ( state ) => BASEURL + ITEMS_URL;
