dfb014192bcbeab5ab2f878e70751de747a1cc15
mszalbach <4-bar-killa@web.de>
2017-08-18 15:14:59 +0200

    - rewritten itemSelector to use semantic-ui components and used better search box instead of a select

--- src/main/js/components/itemSelector/itemSelector.jsx
+++ src/main/js/components/itemSelector/itemSelector.jsx
@@ -1,8 +1,6 @@
 import React from "react";
-import Select from 'react-select';
+import {Dropdown, Grid, Header, Search} from "semantic-ui-react";
 import PropTypes from "prop-types";
-
-import 'react-select/dist/react-select.css';
 
 export default class ItemSelector extends React.Component {
 
@@ -11,26 +9,55 @@ export default class ItemSelector extends React.Component {
         onChange: PropTypes.func.isRequired
     };
 
-    state = {item: ''};
 
-    onChange = ( event ) => {
-        let item = event ? event.value + '' : '';
-        this.setState( {item: item} );
+    componentWillMount() {
+        this.resetComponent()
+    }
 
-        this.props.onChange( item )
+    resetComponent() {
+        this.setState( {isLoading: false, results: [], value: ''} )
+    };
+
+    handleResultSelect = ( e, {result} ) => {
+        this.setState( {value: result.title} );
+        this.props.onChange( result.title );
+
     };
 
 
-    render() {
-        let itemsAsOptions = this.props.items.map( item => {
-            return {value: item.name, label: item.name}
-        } );
+    handleSearchChange = ( e, {value} ) => {
+        this.setState( {isLoading: true, value} );
 
-        return <Select
-            matchPos="start"
-            value={this.state.item}
-            options={itemsAsOptions}
-            onChange={this.onChange}
-        />
+        setTimeout( () => {
+            if ( this.state.value.length < 1 ) {
+                return this.resetComponent()
+            }
+
+            let itemsAsOptions = this.props.items.map( item => {
+                return {title: item.name}
+            } );
+
+            const isMatch = ( result ) => result.title.toLowerCase().startsWith( this.state.value.toLowerCase() );
+
+
+            this.setState( {
+                               isLoading: false,
+                               results: itemsAsOptions.filter( item => isMatch( item ) ),
+                           } )
+        }, 500 )
+    };
+
+    render() {
+        const {isLoading, value, results} = this.state;
+
+        return (
+            <Search fluid
+                    loading={isLoading}
+                    onResultSelect={this.handleResultSelect}
+                    onSearchChange={this.handleSearchChange}
+                    results={results}
+                    value={value}
+            />
+        )
     }
 }
