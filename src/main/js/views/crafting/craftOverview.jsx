cab7d4b313cde427266b66d5de572784df22d2eb
mszalbach <4-bar-killa@web.de>
2017-08-22 11:11:29 +0200

    - changed craft Overview to use semantic ui

--- src/main/js/views/crafting/craftOverview.jsx
+++ src/main/js/views/crafting/craftOverview.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
-import {BootstrapTable, TableHeaderColumn} from "react-bootstrap-table";
-import "react-bootstrap-table/dist/react-bootstrap-table.min.css";
-import IngredientsTable from "./ingredientsTable";
 import PriceText from "../../components/priceText/priceText";
 import PriceTextContainer from "../../components/priceText/priceTextContainer";
 import ItemImage from "../../components/itemImage/itemImageContainer";
+import {Table, TableCell, TableRow} from "semantic-ui-react";
 
 
 export default class CraftOverview extends React.Component {
@@ -14,58 +12,43 @@ export default class CraftOverview extends React.Component {
         items: PropTypes.array.isRequired
     };
 
-    imageFormatter( cell, row ) {
-        return <ItemImage name={row.item} count={cell}/>;
-    }
+    state = {
+        column: null,
+        direction: null,
+    };
 
-    priceEditFormatter( cell ) {
-        return <PriceTextContainer name={cell}/>
-    }
-
-    priceFormatter( cell ) {
-        return <PriceText price={cell}/>
-    }
-
-    highlightedPriceFormatter( cell ) {
-        return <PriceText shouldHighlight={true} price={cell}/>
-    }
-
-    expandComponent( row ) {
-        return <IngredientsTable ingredients={row.ingredients}/>
-    }
 
     render() {
-        const options = {
-            sortName: "item",
-            sortOrder: "asc",
-            expandRowBgColor: 'rgb(242, 255, 163)',
-            expandBy: 'column'
-        };
         let {items} = this.props;
         return (
-            <BootstrapTable data={items}
-                            striped={true}
-                            hover={true}
-                            search={true}
-                            options={options}
-                            expandableRow={() => true}
-                            expandComponent={this.expandComponent}
-            >
-                <TableHeaderColumn dataField="output" dataFormat={this.imageFormatter}
-                                   width="60px">#</TableHeaderColumn>
-                <TableHeaderColumn dataField="item" isKey={true} dataSort={true}
-                                   width="400px">Name</TableHeaderColumn>
-                <TableHeaderColumn dataField="createdBy" dataSort={true}>Profession</TableHeaderColumn>
-                <TableHeaderColumn dataField="cost" dataAlign="right" dataFormat={this.priceFormatter}>Crafting
-                    Cost</TableHeaderColumn>
-                <TableHeaderColumn dataField="item" dataFormat={this.priceEditFormatter}
-                                   dataAlign="right">AH Price</TableHeaderColumn>
-                <TableHeaderColumn dataField="totalCraftingCost" dataAlign="right" dataFormat={this.priceFormatter}>Total
-                    Crafting
-                    Cost</TableHeaderColumn>
-                <TableHeaderColumn dataField="profit" dataAlign="right" dataFormat={this.highlightedPriceFormatter}
-                                   dataSort={true}>Profit</TableHeaderColumn>
-            </BootstrapTable>
+            <Table celled selectable>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell>#</Table.HeaderCell>
+                        <Table.HeaderCell>Name</Table.HeaderCell>
+                        <Table.HeaderCell>Profession</Table.HeaderCell>
+                        <Table.HeaderCell>AH Price</Table.HeaderCell>
+                        <Table.HeaderCell>Total Crafting Cost</Table.HeaderCell>
+                        <Table.HeaderCell>Profit</Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                    {items.map( ( row, index ) =>
+                                    <TableRow key={row.item}>
+                                        <TableCell width="1"><ItemImage name={row.item}
+                                                                        count={row.output}/></TableCell>
+                                        <TableCell>{row.item}</TableCell>
+                                        <TableCell>{row.createdBy}</TableCell>
+                                        <TableCell textAlign="right"><PriceTextContainer
+                                            name={row.item}/></TableCell>
+                                        <TableCell textAlign="right"><PriceText
+                                            price={row.totalCraftingCost}/></TableCell>
+                                        <TableCell textAlign="right"><PriceText shouldHighlight={true}
+                                                                                price={row.profit}/></TableCell>
+                                    </TableRow>
+                    )}
+                </Table.Body>
+            </Table>
         )
     }
 }
