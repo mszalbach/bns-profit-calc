19c9dfd61c2c354e1603f1ae03fb525ae039a36a
Marcel Szalbach <4-bar-killa@web.de>
2017-09-29 21:58:55 +0200

    Update serverStatusContainer.js

--- src/views/menu/serverStatusContainer.js
+++ src/views/menu/serverStatusContainer.js
@@ -1,11 +1,13 @@
 import {connect} from "react-redux";
 import ServerStatus from "./serverStatus";
 import {getServerStatusSelector} from "../../modules/serverStatus/serverStatusSelector";
+import {loadPrices} from "../../modules/prices/pricesReducer";		
+import {loadItems} from "../../modules/items/itemsReducer";
 
 
 export default connect(
     ( state ) => ({
         status: getServerStatusSelector( state )
     }),
-    {}
+    {loadPrices, loadItems}
 )( ServerStatus );
